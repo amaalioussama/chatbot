51efd9e1e6d5f5fb5da856b497d5bca3da060116
amaalioussama <amaaliousama771@gmail.com>
2024-01-19 01:14:04 +0100

    sinal

--- my-app/app/login/page.js
+++ my-app/app/login/page.js
@@ -31,6 +31,7 @@ const Page = () => {
               className="border-b border-white p-2 focus:outline-none bg-transparent text-white font-poppins text-base"
             />
           </div>
+          <br/>
           <div className="mb-4">
             <input
               type="password"
@@ -39,23 +40,34 @@ const Page = () => {
               placeholder="Password"
               className="border-b border-white p-2 focus:outline-none bg-transparent text-white font-poppins text-base"
             />
+            
             <p className="mt-2 text-sm text-gray-500">
+               <br/>
               Forget password? <a href="#" className="font-medium text-white hover:underline">Click here</a>.
             </p>
           </div>
-          <button className="relative inline-flex w-48 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-base font-medium text-white rounded-lg group bg-gradient-to-br from-red-950 via-red-800">
-            <span className="relative px-5 py-2.5 w-48  transition-all ease-in duration-75 bg-black rounded-md group-hover:bg-opacity-0">
-              Login
-            </span>
-          </button>
-          <br />
-          <button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
-            <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-              <path fillRule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clipRule="evenodd"/>
-            </svg>
-            Sign in with Google
-          </button>
-        </div>
+          <button className=" bg-black relative inline-flex w-48 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-base font-medium text-white rounded-lg group ">
+        <span className="relative px-5 py-2.5 w-48 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-gradient-to-br group-hover:from-red-950 group-hover:via-red-800">
+            Login
+           </span>
+         </button>
+
+         
+          <div
+            class="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+            <p
+              class="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
+              OR
+            </p>
+          </div>
+          
+     
+    <button class="px-5 py-2 inline-flex items-center justify-center  border  gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
+        <span>Login with Google</span>
+    </button>
+</div>
+       
       </main>
     </div>
     <Footer />
